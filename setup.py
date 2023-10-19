2939d71f2c5f642945b2d61249f4432705ad5904
Mustafa Faisal <mustafafaisal@Mustafas-MBP.lan>
2023-10-19 15:03:48 +0100

    try fix.

--- setup.py
+++ setup.py
@@ -4,32 +4,14 @@ from setuptools import setup, find_packages  # type: ignore
 import PDDLTaskEncoding
 
 
+# setup.py
 setup(
     name="PDDLTaskEncoding",
     version=PDDLTaskEncoding.__version__,
-    description="PDDL to SMT Encoding Framework",
-    long_description="TODO",
-    long_description_content_type="text/markdown",
-    author="TODO:",
-    author_email="TODO",
-    url="TODO",
+    description="A package for PDDL task encodings",
+    author="Your Name",
+    author_email="your@email.com",
     packages=find_packages(),
-    include_package_data=True,
-    python_requires=">=3.8",
-    install_requires=["z3-solver", "unified-planning"],
-    extras_require={},
-    license="APACHE",
-    keywords="planning logic SMT PDDL",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: Developers",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-    ],
-    entry_points={}
+    install_requires=[ "z3-solver", "unified-planning" ],
+    py_modules=["PDDLTaskEncoding"],
 )
